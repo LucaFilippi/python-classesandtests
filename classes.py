d4acdac5186410ced9be56d0bb86e33f463b13a4
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-06-02 14:37:06 -0300

    classes and math operations

---
+++ classes.py
@@ -0,0 +1,21 @@
+number1 = 5
+number2 = 3.5
+sum = number1 + number2
+
+print(type(number1))
+print(type(number2))
+print(type(sum))
+print(sum)
+
+a = float(number1)
+print(a)
+print(type(a))
+
+
+print(number1 + number2)  # Addition
+print(number1 - number2)  # Subtraction
+print(number1 * number2)  # Multiplication 
+print(number1 / number2)  # Division (returns float)
+print(number1 % number2)   # Modulo (division rest)
+print(number1 // number2) # Floor division (returns integer)
+print(number1 ** number2) # Exponentiation 
---
+++ math_operations.py
@@ -0,0 +1,15 @@
+number1 = 5
+number2 = 3.5
+sum = number1 + number2
+
+print(sum)
+
+
+
+print(number1 + number2)  # Addition
+print(number1 - number2)  # Subtraction
+print(number1 * number2)  # Multiplication 
+print(number1 / number2)  # Division (returns float)
+print(number1 % number2)   # Modulo (division rest)
+print(number1 // number2) # Floor division (returns integer)
+print(number1 ** number2) # Exponentiation 
