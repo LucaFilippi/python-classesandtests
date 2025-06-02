802b081c144bd624b73c93ffb0f375cdcb145f54
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-06-02 14:11:41 -0300

    concatenation example

---
+++ concatenation.py
@@ -0,0 +1,7 @@
+name = input("what is your name?")
+age = input("how old are you?")
+likes = input("what do you do in your freetime?")
+
+print(name)
+print (age)
+print ("My name is "+name +", I am "+ age + " years old, and i like " +likes +" very much")
